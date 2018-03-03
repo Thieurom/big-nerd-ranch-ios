76a7c9704c1d538df02accd3ea05b70ecc14abfa
Doan Le Thieu <thieurom@gmail.com>
2018-03-03 09:53:09 +0700

    Start Quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Doan Le Thieu on 3/3/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers = [
+        "14",
+        "Montplier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex = 0
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+
