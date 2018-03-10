4e8da25046b5c8ef168fde275670f7fa196ad980
Doan Le Thieu <thieurom@gmail.com>
2018-03-10 15:08:26 +0700

    [WorldTrotter]: Gold Challenge - Dropping Pins

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -14,6 +14,8 @@ class MapViewController: UIViewController {
     var mapView: MKMapView!
     var locationManager: CLLocationManager!
     var userLocation: CLLocation?
+    var annotations = [MKPointAnnotation]()
+    var pinIndex = 0
     
     override func loadView() {
         mapView = MKMapView()
@@ -22,6 +24,8 @@ class MapViewController: UIViewController {
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
+        
+        // MARK: Segmented Control to select map type
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
@@ -39,21 +43,58 @@ class MapViewController: UIViewController {
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
         
+        // MARK: Show user's current location button
         
         let showLocationButton = UIButton(type: .system)
         showLocationButton.frame.size = CGSize(width: 50, height: 50)
         showLocationButton.setBackgroundImage(UIImage(named: "compass"), for: .normal)
-        print(showLocationButton.frame)
         showLocationButton.layer.cornerRadius = 25
         showLocationButton.translatesAutoresizingMaskIntoConstraints = false
         showLocationButton.addTarget(self, action: #selector(MapViewController.showLocationButtonTapped(_:)), for: .touchUpInside)
         view.addSubview(showLocationButton)
         
-        let showBtnTopConstraint = showLocationButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        let showBtnTrailingConstraint = showLocationButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
         let showBtnBottomConstraint = showLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
         
-        showBtnTopConstraint.isActive = true
+        showBtnTrailingConstraint.isActive = true
         showBtnBottomConstraint.isActive = true
+        
+        // MARK: Drop pins for 3 pre-defined locations
+        
+        let showPinButton = UIButton(type: .system)
+        showPinButton.frame.size = CGSize(width: 50, height: 50)
+        showPinButton.setBackgroundImage(UIImage(named: "pin"), for: .normal)
+        showPinButton.layer.cornerRadius = 25
+        showPinButton.translatesAutoresizingMaskIntoConstraints = false
+        showPinButton.addTarget(self, action: #selector(MapViewController.showPinButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(showPinButton)
+        
+        let pinBtnTrailingConstraint = showPinButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        let pinBtnBottomConstraint = showPinButton.bottomAnchor.constraint(equalTo: showLocationButton.topAnchor, constant: -5)
+        
+        pinBtnTrailingConstraint.isActive = true
+        pinBtnBottomConstraint.isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let annotation1 = MKPointAnnotation()
+        annotation1.coordinate = CLLocationCoordinate2D(latitude: 18.679585, longitude: 105.681335)
+        annotation1.title = "Nghe An"
+        
+        let annotation2 = MKPointAnnotation()
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 16.047079, longitude: 108.206230)
+        annotation2.title = "Da Nang"
+        
+        let annotation3 = MKPointAnnotation()
+        annotation3.coordinate = CLLocationCoordinate2D(latitude: 10.762622, longitude: 106.660172)
+        annotation3.title = "Ho Chi Minh City"
+        
+        annotations.append(annotation1)
+        annotations.append(annotation2)
+        annotations.append(annotation3)
+        mapView.addAnnotations(annotations)
     }
     
     @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
@@ -74,16 +115,23 @@ class MapViewController: UIViewController {
             locationManager.requestWhenInUseAuthorization()
         } else {
             if let userLocation = userLocation {
-                centerMapWithLocation(userLocation)
+                centerMapWithLocationCoordinate(userLocation.coordinate)
             }
         }
     }
     
-    func centerMapWithLocation(_ location: CLLocation) {
-        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 500, 500)
+    @objc func showPinButtonTapped(_ sender: UIButton) {
+        centerMapWithLocationCoordinate(annotations[pinIndex].coordinate)
+        pinIndex = (pinIndex + 1) % annotations.count
+    }
+    
+    func centerMapWithLocationCoordinate(_ locationCoordinate: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegionMakeWithDistance(locationCoordinate, 500, 500)
         mapView.setRegion(region, animated: true)
     }
 }
+
+// MARK: - CLLocationManagerDelegate
 
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -93,14 +141,37 @@ extension MapViewController: CLLocationManagerDelegate {
     }
 }
 
+// MARK: - MKMapViewDelegate
+
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         if let newLocation = userLocation.location {
             if self.userLocation == nil {
-                centerMapWithLocation(newLocation)
+                centerMapWithLocationCoordinate(newLocation.coordinate)
             }
             
             self.userLocation = newLocation
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Don't show pin for user's current location
+        if annotation.coordinate.latitude == userLocation?.coordinate.latitude &&
+            annotation.coordinate.longitude == userLocation?.coordinate.longitude {
+            return nil
+        }
+        
+        var view = mapView.dequeueReusableAnnotationView(withIdentifier: "pin") as? MKPinAnnotationView
+        
+        if view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            view?.canShowCallout = true
+            view?.animatesDrop = true
+            view?.pinTintColor = MKPinAnnotationView.redPinColor()
+        } else {
+            view?.annotation = annotation
+        }
+        
+        return view
     }
 }
