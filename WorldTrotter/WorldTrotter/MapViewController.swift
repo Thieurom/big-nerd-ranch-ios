b45e0d90abe26e1ddac50857a35aa95d6c319b74
Doan Le Thieu <thieurom@gmail.com>
2018-03-10 10:08:14 +0700

    [WorldTrotter]: Silver Challenge - User's Location

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,10 +12,16 @@ import MapKit
 class MapViewController: UIViewController {
     
     var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    var userLocation: CLLocation?
     
     override func loadView() {
         mapView = MKMapView()
+        mapView.delegate = self
         view = mapView
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
@@ -32,6 +38,22 @@ class MapViewController: UIViewController {
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
+        
+        
+        let showLocationButton = UIButton(type: .system)
+        showLocationButton.frame.size = CGSize(width: 50, height: 50)
+        showLocationButton.setBackgroundImage(UIImage(named: "compass"), for: .normal)
+        print(showLocationButton.frame)
+        showLocationButton.layer.cornerRadius = 25
+        showLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        showLocationButton.addTarget(self, action: #selector(MapViewController.showLocationButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(showLocationButton)
+        
+        let showBtnTopConstraint = showLocationButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        let showBtnBottomConstraint = showLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        
+        showBtnTopConstraint.isActive = true
+        showBtnBottomConstraint.isActive = true
     }
     
     @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
@@ -44,6 +66,41 @@ class MapViewController: UIViewController {
             mapView.mapType = .satellite
         default:
             break
+        }
+    }
+    
+    @objc func showLocationButtonTapped(_ sender: UIButton) {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        } else {
+            if let userLocation = userLocation {
+                centerMapWithLocation(userLocation)
+            }
+        }
+    }
+    
+    func centerMapWithLocation(_ location: CLLocation) {
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 500, 500)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            self.mapView.showsUserLocation = true
+        }
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let newLocation = userLocation.location {
+            if self.userLocation == nil {
+                centerMapWithLocation(newLocation)
+            }
+            
+            self.userLocation = newLocation
         }
     }
 }
