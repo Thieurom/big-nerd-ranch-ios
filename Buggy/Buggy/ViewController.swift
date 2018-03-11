a061f84dd76e038702ae3bcfbc4a9c0f8abeb0dd
Doan Le Thieu <thieurom@gmail.com>
2018-03-11 15:00:04 +0700

    Add Buggy app - Debugging (chap 9)

---
+++ Buggy/Buggy/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Buggy
+//
+//  Created by Doan Le Thieu on 3/11/18.
+//  Copyright © 2018 Doan Le Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
+    
+}
+
