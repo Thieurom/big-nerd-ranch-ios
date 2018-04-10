95deff47eb0bffa5efd3a5badf122688801f34e9
Doan Le Thieu <thieurom@gmail.com>
2018-04-10 09:21:40 +0700

    [Photorama]: Silver Challenge - Updated item sizes

--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -71,3 +71,20 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
     }
 }
 
+extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow = CGFloat(4)
+        var paddingSpace = CGFloat(0)
+        
+        if let collectionViewFlowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+            paddingSpace += collectionViewFlowLayout.minimumInteritemSpacing * (itemsPerRow - 1)
+            paddingSpace += collectionViewFlowLayout.sectionInset.left + collectionViewFlowLayout.sectionInset.right
+        }
+        
+        let itemWidth = ((collectionView.bounds.width - paddingSpace) / itemsPerRow).rounded(.down)
+        
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+}
+
